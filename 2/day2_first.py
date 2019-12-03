894536c7401a83734e66f14a5798b756e04efb10
Tanguy Soto <tanguy.soto@gmail.com>
2019-12-03 16:14:13 +0100

    Day2 structure

---
+++ 2/day2_first.py
@@ -0,0 +1,19 @@
+import os
+import math
+
+"""
+https://adventofcode.com/2019
+
+Day 2 - First puzzle
+
+@author Tanguy Soto
+@date 03/12/2019 - Darmstadt
+"""
+
+
+if __name__ == '__main__':
+    INPUT_FILE_ABSPATH = os.path.abspath("first_input.txt")
+
+    answer = None
+
+    print("Day2 - first puzzle answer:\n{}".format(answer))
