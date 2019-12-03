fbc17c0a61e85175bf67b3c5edf4e2095660891e
Tanguy Soto <tanguy.soto@gmail.com>
2019-12-03 17:40:35 +0100

    Day3 structure

---
+++ 3/day3_first.py
@@ -0,0 +1,18 @@
+import os
+
+"""
+https://adventofcode.com/2019
+
+Day 3 - First puzzle
+
+@author Tanguy Soto
+@date 03/12/2019 - Darmstadt
+"""
+
+
+if __name__ == '__main__':
+    INPUT_FILE_ABSPATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                      "first_input.txt")
+    answer = None
+
+    print("Day3 - first puzzle answer:\n{}".format(answer))
