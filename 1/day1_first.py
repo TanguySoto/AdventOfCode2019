75e7ef5f1f270c3c29aa3ec9fccb1354c5b9e998
Tanguy Soto <tanguy.soto@gmail.com>
2019-12-03 14:52:34 +0100

    Day1 structure

---
+++ 1/day1_first.py
@@ -0,0 +1,25 @@
+import os
+
+"""
+https://adventofcode.com/2019
+
+Day 1 - First puzzle
+
+@author Tanguy Soto
+@date 03/12/2019 - Darmstadt
+"""
+
+
+def process_module(module_mass):
+    pass
+
+def process_spacecraft(spacecraft_file_abspath):
+    pass
+
+
+if __name__ == '__main__':
+    INPUT_FILE_ABSPATH = os.path.abspath("first_input.txt")
+
+    answer = process_spacecraft(INPUT_FILE_ABSPATH)
+
+    print("Day1 - first puzzle answer:\n{}".format(answer))
